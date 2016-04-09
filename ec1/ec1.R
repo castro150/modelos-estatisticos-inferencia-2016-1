4a0bbadeda0c93d9d1bcf966dea17ed56a7c1e8b
castro150 <rafaelcarneiroget@hotmail.com>
2016-04-09 00:00:45 -0300

    Correção de código

--- ec1/ec1.R
+++ ec1/ec1.R
@@ -30,10 +30,18 @@ s <- sqrt(sum((BMI-x_bar)^2)/(n-1));
 #t crítico
 t_alpha <- qt(alpha/2, n-1);
 
+#T0
+t0 <- (x_bar - mu)/(s/sqrt(n));
+
 #Intervalo de confiança
 inter_min <- x_bar + (s*t_alpha / (sqrt(n)));
 inter_max <- x_bar - (s*t_alpha / (sqrt(n)));
 
+power.t.test(n=n,
+              delta=size_effect,
+              sd=s,
+              sig.level=0.05,
+              type = "one.sample");
 message('Valor de mu: ', mu);
 message('Valor de alpha: ', alpha);
 message('Número de amostras: ', n);
@@ -41,5 +49,6 @@ message('Média amostral: ', x_bar);
 message('Desvio padrão: ', s);
 message('Tamanho de efeito: ', size_effect);
 message('T crítico: ', t_alpha);
+message('T0: ', t0);
 message('Intervalo de confiança: ', inter_min, ' a ', inter_max);
 
