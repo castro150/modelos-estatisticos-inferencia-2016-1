bbf959ead69e11d9c71689129966a109e41e9607
castro150 <rafaelcarneiroget@hotmail.com>
2016-04-14 20:59:32 -0300

    Teste de potência e gráfico de normalidade

--- ec2/ec2.R
+++ ec2/ec2.R
@@ -21,15 +21,55 @@ boxplot(bmiEngSis, bmiPpgee, ylab="BMI",
          names=c("Eng. Sistemas","PPGEE"), col = "lightgray",
          main="BMI para Eng. Sistemas e PPGEE (Pós-Graduação)");
 
-# Executar teste de igualdade de variancia
-var.test(bmiEngSis, bmiPpgee, alternitive="two.sided",conf.level=0.95)
+# qqPlot para testar a normalidade dos residuos
+require("car");
+resid <- c(bmiEngSis - mean(bmiEngSis), bmiPpgee - mean(bmiPpgee));
+qqPlot(resid,
+       pch=16,
+       cex=1.5,
+       las=1);
+# É aproximadamente uma reta. Então a normalidade é aceitável.
 
-# Variancias bem diferentes
+# Executar teste de igualdade de variancia
+var.test(bmiEngSis, bmiPpgee, alternitive="two.sided",conf.level=0.95);
+# Como p-value > alpha, a hipótese nula não foi rejeitada. Sendo
+# assim, não se pode falar com certeza que as variâncias não são iguais.
+
+# Test t com variancia igual ou não
 t.test(bmiEngSis, bmiPpgee,
       alternative = "two.sided",
       mu = 0,
-      var.equal = FALSE, # Mudar aqui pelo resultado do teste
-      conf.level = 0.95)
+      var.equal = TRUE, # Mudar aqui pelo resultado do teste
+      conf.level = 0.95);
+#P-Value > alpha não rejeita a hipótese nula.
 
-# Estimar tamanho de efeito
+
+# Estimar tamanho de efeito = 1.5
 # Calcular a potência do teste
+require("pwr");
+pwr.t2n.test(n1 = 13,
+             n2 = 28,
+             d = 1.5,
+             sig.level = 0.05,
+             alternative = "two.sided");
+# Problema que o delta é alto. Reduzindo delta.
+pwr.t2n.test(n1 = 13,
+             n2 = 28,
+             d = 0.8,
+             sig.level = 0.05,
+             alternative = "two.sided");
+# Com o delta reduzido, a potência também. Obter a quantidade
+# de amostras da engenharia de sistemas para ter uma motência
+# de 80%.
+pwr.t2n.test(power = 0.8,
+             n2 = 28,
+             d = 0.8,
+             sig.level = 0.05,
+             alternative = "two.sided");
+
+# Dá pra fazer o power.t.test com o menor tamanho amostral e maior sd
+power.t.test(n=13,
+             delta=1.5,
+             sd=sd(bmiEngSis),
+             sig.level=0.05,
+             type = "two.sample");
