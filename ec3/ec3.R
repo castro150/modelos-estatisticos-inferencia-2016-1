32a405b89a5a798a879f9bbd1cd230a798594b92
castro150 <rafaelcarneiroget@hotmail.com>
2016-04-16 12:14:56 -0300

    Codigo com boxplot

--- ec3/ec3.R
+++ ec3/ec3.R
@@ -2,7 +2,7 @@
 #          ESTUDO DE CASO 3               #
 #                                         #
 #Equipe: Gustavo Vieira Costa             #
-#        Marcus Vinpicius do Carmo Bastos #
+#        Marcus Vinícius do Carmo Bastos  #
 #        Rafael Carneiro de Castro        #
 #        Thaís Matos Acácio               #
 ###########################################
@@ -10,5 +10,32 @@
 #Ler dados de entrada
 dados <- read.table("dados.csv", header=TRUE, sep=",");
 aggdata <- aggregate(dados$Peso~dados$Fonte,data=dados,FUN=mean);
-summary(aggdata)
+summary(aggdata);
+
+#Separando
+estimados <- c(0);
+ei <- 1;
+medidos <- c(0);
+mi <- 1;
+for (i in seq(1:22)) {
+  if (dados$Fonte[i] == 'Estimado') {
+    estimados[ei] <- dados$Peso[i];
+    ei <- ei + 1;
+  } else {
+    medidos[mi] <- dados$Peso[i];
+    mi <- mi + 1;
+  }
+}
+
+#Boxplot Pesos Estimados e Medidos em Balança
+boxplot(estimados, medidos, ylab="Peso (kg)",
+        names=c("Estimados","Medidos"), col = "lightgray",
+        main="Pesos Estimados e Medidos em Balança");
+
+#Boxplot Diferença dos Pesos Estimados e Medidos
+diffPesos <- estimados - medidos;
+boxplot(diffPesos, ylab="Peso (kg)",
+        names=c("Diferença"), col = "lightgray",
+        main="Diferença dos Pesos Estimados e Medidos");
+
 t.test(dados$Peso~dados$Fonte, paired=T, data=aggdata);
